7966b81ecc8afbd9c72babe5e2e086ef39f93aeb
kayza <kayzakizy@gmail.com>
2022-08-10 19:22:58 -0300

    :sparkler: Aulas de teste  :sparkler:

--- src/shared/infra/typeorm/ormconfig.example.ts
+++ src/shared/infra/typeorm/ormconfig.example.ts
@@ -1,24 +1,21 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
 //docker-compose exec app node --require ts-node/register ./node_modules/typeorm/cli.js migration:run -d src/database
 //yarn typeorm migration:create src/database/migrations/CreateCategories
 //yarn typeorm migration:revert -d src/database/ormconfig.ts
 
-const AppDataSource = new DataSource({
+import "reflect-metadata";
+import { DataSource, DataSourceOptions } from "typeorm";
+
+const infoConnection: DataSourceOptions = {
   type: "postgres",
-  host: "nome do container do docker-compose.yml",
+  host: process.env.NODE_ENV !== "test" ? "database_ignite" : "localhost",
   port: 5432,
   username: "docker",
   password: "ignite",
   database: process.env.NODE_ENV === "test" ? "rentx_test" : "rentx",
   entities: ["src/modules/**/infra/typeorm/entities/*.ts"],
-  migrations: ["src/shared/infra/typeorm/migrations/*.ts"]
-});
+  migrations: ["src/shared/infra/typeorm/migrations/*.ts"],
+};
 
-AppDataSource.initialize()
-  .then(async () => {
-    console.log("Initializing the database...");
-  })
-  .catch((err) => console.log(err));
+const AppDataSource = new DataSource(infoConnection);
 
 export default AppDataSource;
