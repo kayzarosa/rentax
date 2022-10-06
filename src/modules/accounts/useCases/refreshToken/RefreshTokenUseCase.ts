5b1a8d598612a8682214dbec29483d52ccac4d40
kayza <kayzakizy@gmail.com>
2022-10-06 18:08:59 -0300

    Ajustes refresh token

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -11,6 +11,11 @@ interface IPayload {
   email: string;
 }
 
+interface ITokenResponse {
+  token: string;
+  refresh_token: string;
+}
+
 @injectable()
 class RefreshTokenUseCase {
   constructor(
@@ -21,7 +26,7 @@ class RefreshTokenUseCase {
     private dayDateProvider: IDateProvider
   ) {}
 
-  async execute(token: string): Promise<string> {
+  async execute(token: string): Promise<ITokenResponse> {
     const { email, sub } = verify(token, auth.secrete_refresh_token) as IPayload;
 
     const user_id = sub;
@@ -49,7 +54,15 @@ class RefreshTokenUseCase {
       expires_date: this.dayDateProvider.addDays(auth.expires_refresh_token_days),
     });
 
-    return refresh_token;
+    const newToken = sign({}, auth.secrete_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_token,
+    });
+
+    return {
+      refresh_token,
+      token: newToken
+    };
   }
 }
 
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -4,8 +4,6 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import auth from "@config/auth";
 
-import UsersTokenRepository from "@modules/accounts/infra/typeorm/repositories/UsersTokenRepository";
-
 interface IPayload {
   sub: string;
 }
@@ -17,8 +15,6 @@ export async function ensureAuthenticated(
 ) {
   const authHeader = request.headers.authorization;
 
-  const userTokensRepository = new UsersTokenRepository();
-
   if (!authHeader) {
     throw new AppError("Token missing", 401);
   }
@@ -28,17 +24,8 @@ export async function ensureAuthenticated(
   try {
     const { sub: user_id } = verify(
       token,
-      auth.secrete_refresh_token
+      auth.secrete_token
     ) as IPayload;
-
-    const user = await userTokensRepository.findByUserIdAndRefreshToken(
-      user_id,
-      token
-    );
-
-    if (!user) {
-      throw new AppError("User does not exists!", 401);
-    }
 
     request.user = {
       id: user_id,
